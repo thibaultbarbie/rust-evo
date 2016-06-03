5d3c4861f20cd9a01259ee9805333704c25571a0
Steve Klabnik <steve@steveklabnik.com>
2016-06-03 10:00:42 -0400

    Run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-/* Author : Thibault Barbie
-
-A simple evolutionary algorithm written in Rust.
-
-*/
+// Author : Thibault Barbie
+//
+// A simple evolutionary algorithm written in Rust.
+//
+//
 
 extern crate rand;
 
@@ -10,34 +10,37 @@ use rand::Rng;
 use std::collections::HashMap;
 
 fn main() {
-    let target : String ="METHINKS IT IS LIKE A WEASEL".to_string();
-    println!("{}",target);
+    let target: String = "METHINKS IT IS LIKE A WEASEL".to_string();
+    println!("{}", target);
 
-    let mut parent : String="".to_string();
+    let mut parent: String = "".to_string();
     generate_first_sentence(&mut parent);
-    println!("{}",parent);
+    println!("{}", parent);
 
     let nb_copy = 400;
-    let mutation_rate : f64 = 0.05;
-    let mut counter=0;
-    
-    while fitness(&target, &parent)!=0 {
-        counter+=1;
-        let mut sentences=HashMap::new();
-        let mut f_min : u32 = 30;
-        for _ in 0..nb_copy {
-            let sentence=mutate(&mut parent, mutation_rate);
-            let f=fitness(&target, &sentence);
-            sentences.insert(f,sentence);
+    let mutation_rate: f64 = 0.05;
+    let mut counter = 0;
 
-            if f<f_min {f_min=f}
+    while fitness(&target, &parent) != 0 {
+        counter += 1;
+        let mut sentences = HashMap::new();
+        let mut f_min: u32 = 30;
+        for _ in 0..nb_copy {
+            let sentence = mutate(&mut parent, mutation_rate);
+            let f = fitness(&target, &sentence);
+            sentences.insert(f, sentence);
+
+            if f < f_min {
+                f_min = f
+            }
         }
-        
-        if fitness(&target, &parent)>f_min {
+
+        if fitness(&target, &parent) > f_min {
             match sentences.get(&f_min) {
                 Some(s) => {
-                    parent=s.clone();
-                    println!("{} : {}",parent,counter);},
+                    parent = s.clone();
+                    println!("{} : {}", parent, counter);
+                }
                 None => println!("Error, fitness minimum but no sentence."),
             }
         }
@@ -45,30 +48,31 @@ fn main() {
 }
 
 
-fn fitness(target : &String, sentence : &String) -> u32 {
+fn fitness(target: &String, sentence: &String) -> u32 {
     let mut fitness = 0;
-    for (c1, c2) in target.chars().zip(sentence.chars())
-    {
-        if c1!=c2 { fitness+=1 };
+    for (c1, c2) in target.chars().zip(sentence.chars()) {
+        if c1 != c2 {
+            fitness += 1
+        };
     }
     fitness
 }
 
-fn mutate(sentence : &mut String, mutation_rate : f64) -> String {
-    let mut rng=rand::thread_rng();
-    
-    let mut mutation : String ="".to_string();
+fn mutate(sentence: &mut String, mutation_rate: f64) -> String {
+    let mut rng = rand::thread_rng();
+
+    let mut mutation: String = "".to_string();
     for c in sentence.chars() {
-        match mutation_rate>rng.gen_range(0f64,1f64) {
+        match mutation_rate > rng.gen_range(0f64, 1f64) {
             false => mutation.push(c),               // no mutation
-            true  => mutation.push(random_char()),   // mutation
+            true => mutation.push(random_char()),   // mutation
         }
     }
     mutation
 }
 
 
-fn generate_first_sentence (parent : &mut String) {
+fn generate_first_sentence(parent: &mut String) {
     for _ in 0..28 {
         parent.push(random_char());
     }
@@ -77,6 +81,6 @@ fn generate_first_sentence (parent : &mut String) {
 fn random_char() -> char {
     match rand::thread_rng().gen_range('A' as u8, '\\' as u8) as char {
         '[' => ' ',
-        c @ _ => c
+        c @ _ => c,
     }
 }
