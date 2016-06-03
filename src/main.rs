b3771a04c91a985fce2c19a540a31174b43e3250
Fouassier Raphaël <ntimeu@ntimeu.fr>
2016-06-03 11:35:13 +0200

    Code: various code lints & doctrings.

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,91 @@
-/* Author : Thibault Barbie
-
-A simple evolutionary algorithm written in Rust.
-
-*/
+//! Author : Thibault Barbie
+//!
+//! A simple evolutionary algorithm written in Rust.
 
 extern crate rand;
 
-use rand::Rng;
-use std::collections::HashMap;
+use std::collections::{HashMap};
+use rand::{Rng};
 
 fn main() {
-    let target : String ="METHINKS IT IS LIKE A WEASEL".to_string();
-    println!("{}",target);
-
-    let mut parent : String="".to_string();
-    generate_first_sentence(&mut parent);
-    println!("{}",parent);
-
+    let target: String = String::from("METHINKS IT IS LIKE A WEASEL");
+    let mut parent: String = "".to_string();
     let nb_copy = 400;
     let mutation_rate : f64 = 0.05;
     let mut counter=0;
+
+    generate_first_sentence(&mut parent);
+
+    println!("{}", target);
+    println!("{}", parent);
     
-    while fitness(&target, &parent)!=0 {
+    while fitness(&target, &parent) != 0 {
+        let mut sentences: HashMap<u32, String> = HashMap::new();
+        let mut f_min: u32 = 30;
+
         counter+=1;
-        let mut sentences=HashMap::new();
-        let mut f_min : u32 = 30;
-        for i in 0..nb_copy {
-            let sentence=mutate(&mut parent, mutation_rate);
-            let f=fitness(&target, &sentence);
+
+        for _ in 0..nb_copy {
+            let sentence = mutate(&mut parent, mutation_rate);
+            let f = fitness(&target, &sentence);
             sentences.insert(f,sentence);
 
-            if f<f_min {f_min=f}
+            if f<f_min { f_min = f; }
         }
         
-        if fitness(&target, &parent)>f_min {
+        if fitness(&target, &parent) > f_min {
             match sentences.get(&f_min) {
                 Some(s) => {
-                    parent=s.clone();
-                    println!("{} : {}",parent,counter);},
-                None => println!("Error, fitness minimum but no sentence."),
+                    parent = s.clone();
+                    println!("{} : {}", parent, counter);
+                },
+                None => panic!("Error, fitness minimum but no sentence."),
             }
         }
     }
 }
 
-
-fn fitness(target : &String, sentence : &String) -> u32 {
+/// Computes the fitness of a sentence against a target string.
+fn fitness(target: &String, sentence: &String) -> u32 {
     let mut fitness = 0;
-    for (c1, c2) in target.chars().zip(sentence.chars())
-    {
-        if c1!=c2 { fitness+=1 };
+
+    for (c1, c2) in target.chars().zip(sentence.chars()) {
+        if c1 != c2 {
+            fitness += 1;
+        }
     }
+
     fitness
 }
 
-fn mutate(sentence : &mut String, mutation_rate : f64) -> String {
-    let mut rng=rand::thread_rng();
+/// Mutation algorithm.
+///
+/// It mutates each character of a string, according to a `mutation_rate`.
+/// Please note that for full usefullness, `mutation_rate` should be between
+/// 0 and 1.
+fn mutate(sentence: &mut String, mutation_rate: f64) -> String {
+    let mut rng = rand::thread_rng();
     
-    let mut mutation : String ="".to_string();
+    let mut mutation: String = "".to_string();
+
     for c in sentence.chars() {
-        match mutation_rate>rng.gen_range(0f64,1f64) {
-            false => mutation.push(c),               // no mutation
-            true  => mutation.push(random_char()),   // mutation
+        if mutation_rate > rng.gen_range(0f64, 1f64) {
+            mutation.push(c);
+        } else {
+            mutation.push(random_char());
         }
     }
     mutation
 }
 
-
-fn generate_first_sentence (parent : &mut String) {
-    for i in 0..28 {
+/// Generates a random sentence of length 28 from completly random chars.
+fn generate_first_sentence(parent: &mut String) {
+    for _ in 0..28 {
         parent.push(random_char());
     }
 }
 
+/// Generates a random char (between 'A' and '\\').
 fn random_char() -> char {
     match rand::thread_rng().gen_range('A' as u8, '\\' as u8) as char {
         '[' => ' ',
